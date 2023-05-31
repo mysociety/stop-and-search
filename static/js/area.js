cacabeb37cc38b1285b12cdfd6276a5a0fff0414
Graeme Porteous <graeme@rgbp.co.uk>
2023-05-31 23:57:00 +0100

    Update area page HTML and JS
    - Fix a few oddities
    - Combine ratio sections
    - Show metric ratios only if a value has been selected

--- static/js/area.js
+++ static/js/area.js
@@ -2,29 +2,52 @@ import $ from '../vendor/jquery/js/jquery.esm.js'
 
 $(function(){
     let dropdownMetric = $('.dropdown-metric')
-    let metricContent = $('.js-dinamic-metric-content')
+    let metricContent = $('.js-dynamic-metric-content')
     let dropdownYear = $('#year')
     let resetMetric = $('#reset-metric')
 
     metricContent.hide()
 
     dropdownMetric.on('change', function() {
-        metricContent.fadeIn(500)
         updateValues()
     })
 
     resetMetric.on( "click", function() {
         $("#year").val('')
-        $("#object-of-search").val('')
+        $("#object").val('')
         $("#legislation").val('')
         $("#outcome").val('')
         updateValues()
     })
 })
 
-function updateValues($param) {
-    $('.metric-legislation').text($("#legislation option:selected").text())
-    $('.metric-object').text($("#object-of-search option:selected").text())
-    $('.metric-outcome').text($("#outcome option:selected").text())
+function updateValues() {
+    const object = $("#object option:selected").text()
+    $('.metric-object .js-value').text(object)
+
+    if (object != 'All') {
+      $('.metric-object.js-dynamic-metric-content').fadeIn(500)
+    } else {
+      $('.metric-object.js-dynamic-metric-content').fadeOut(500)
+    }
+
+    const legislation = $("#legislation option:selected").text()
+    $('.metric-legislation .js-value').text(legislation)
+
+    if (legislation != 'All') {
+      $('.metric-legislation.js-dynamic-metric-content').fadeIn(500)
+    } else {
+      $('.metric-legislation.js-dynamic-metric-content').fadeOut(500)
+    }
+
+    const outcome = $("#outcome option:selected").text()
+    $('.metric-outcome .js-value').text(outcome)
+
+    if (outcome != 'All') {
+      $('.metric-outcome.js-dynamic-metric-content').fadeIn(500)
+    } else {
+      $('.metric-outcome.js-dynamic-metric-content').fadeOut(500)
+    }
+
     $('.metric-year').text($("#year option:selected").text())
 }
