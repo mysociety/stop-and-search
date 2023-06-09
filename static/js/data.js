ecef66c169a8dc2f1fe4c4fe24888910df54994b
Graeme Porteous <graeme@rgbp.co.uk>
2023-06-09 13:36:36 +0100

    Rework data loading, updating area figures and copy

--- static/js/data.js
+++ static/js/data.js
@@ -28,12 +28,12 @@ async function downloadAndCacheDatabase() {
   if (cachedVersion && currentVersion == cachedVersion.data) {
     const cachedFile = await db.get('files', databaseURL)
     if (cachedFile) {
-      console.log('USE CACHED FILE')
+      console.debug('USE CACHED FILE')
       return cachedFile.data
     }
   }
 
-  console.log('FETCH REMOTE FILE')
+  console.debug('FETCH REMOTE FILE')
   const response = await fetch(databaseURL)
   const arrayBuffer = await response.arrayBuffer()
   await db.put('files', { id: databaseURL, data: arrayBuffer })
@@ -99,69 +99,116 @@ async function getData(_where = null, _limit = null) {
 $(function() {
   $('.js-area-name').text(area.name)
 
-  function updateData() {
+  function fetchData() {
     const year = $("#year").val()
+    const query = (year != '') ? `date=${year}` : null
 
+    // Fetch all data for an area for a given year
+    getData(query).then(function (data) {
+      updateData(data)
+      updatePlots(data)
+    })
+  }
+
+  function updateData(data) {
     $('.js-data').each(function () {
+      console.debug('---------')
+
       const metric = $(this).data('metric')
+      console.debug('metric', metric)
+
+      const metricCategory = $(this).data('metric-category') || metric
+      console.debug('metricCategory', metricCategory)
+
+      const metricData = data.filter(obj => obj.metric === metric && (obj.metric_category === 'NA' || obj.metric_category === metricCategory))
+      console.debug('metricData', metricData)
+
+      const valueTypes = [...new Set(metricData.map(obj => obj.value_type))]
+      const type = (valueTypes.length > 1) ? $(this).data('type') : valueTypes[0]
+
+      const metricTypeData = (valueTypes.length > 1) ?
+        metricData.filter(obj => obj.value_type === type) :
+        metricData
+      const metricTypeSum = metricTypeData.reduce((acc, obj) => acc + obj.value, 0)
+      const metricTypeCount = metricTypeData.length
+      console.debug('metricTypeData', type, metricTypeData, metricTypeSum, metricTypeCount)
+
       const ethnicity = $(this).data('ethnicity')
+      const ethnicityData = metricTypeData.filter(obj => obj.ethnicity === ethnicity && typeof obj.value === 'number')
+      const ethnicitySum = ethnicityData.reduce((acc, obj) => acc + obj.value, 0)
+      const ethnicityCount = ethnicityData.length
+      console.debug('ethnicity', ethnicity, ethnicityData, ethnicitySum, ethnicityCount)
 
-      const baseQuery = `metric="${metric}" AND ethnicity="${ethnicity}"`
-      const query = (year != '') ? `date=${year} AND ${baseQuery}` : baseQuery
+      const ethnicityOtherData = metricTypeData.filter(obj => obj.ethnicity !== ethnicity && typeof obj.value === 'number')
+      const ethnicityOtherSum = ethnicityOtherData.reduce((acc, obj) => acc + obj.value, 0)
+      const ethnicityOtherCount = ethnicityOtherData.length
+      console.debug('ethnicityOther', `!${ethnicity}`, ethnicityOtherData, ethnicityOtherSum, ethnicityOtherCount)
 
-      const $this = $(this)
+      let value
+      const format = $(this).data('format') || type
+      switch (format) {
+        case 'comparison-percentage':
+          value = (((ethnicitySum / ethnicityCount) / (metricTypeSum / metricTypeCount)) * 50).toFixed()
+          value = `${value}%`
+          break
+        case 'percentage':
+          value = (ethnicitySum / ethnicityCount).toFixed(2)
+          value = `${value}%`
+          break
+        case 'ratio-as-percentage':
+          value = (((metricTypeSum / metricTypeCount) - 1) * 100)
+          if (value < 0) { value = (1 - (1 / (metricTypeSum / metricTypeCount))) * -100 }
+          value = `${value.toFixed()}%`
+          break
+        case 'ratio-as-more-or-less':
+          value = (((metricTypeSum / metricTypeCount) - 1) * 100)
+          if (value == 0) { value = 'as'}
+          else if (value < 0) { value = 'less' }
+          else { value = 'more' }
+          break
+        case 'ratio':
+          value = (metricTypeSum / metricTypeCount).toFixed(2)
+          break
+        case 'frequency':
+          value = ethnicitySum.toLocaleString()
+          break
+      }
 
-      getData(query).then(function (data) {
-        let sum = 0
-        let count = 0
-        let value
+      console.debug('value', value)
 
-        data.forEach((obj) => {
-          if (typeof obj.value === 'number') {
-            sum += obj.value
-            count++
-          }
-        })
-
-        if (data[0].value_type == 'percentage') {
-          sum = sum / count
-          value = (sum).toFixed(2)
-        } else if (data[0].value_type == 'frequency') {
-          value = sum.toLocaleString()
-        }
-
-        if ($this.data('target') == 'width') {
-          $this.css('width', `${value}%`)
-        } else {
-          $this.text(value)
-        }
-      })
+      const target = $(this).data('target')
+      if (target == 'height') { $(this).css('height', value) }
+      else if (target == 'width') { $(this).css('width', value) }
+      else { $(this).text(value) }
     })
   }
 
-  function updatePlots() {
-    const year = $("#year").val()
-
-    const promises = $('.js-plot').map(function () {
-      if (year == '') { return }
-      const query = `date=${year} AND value_type="ratio"` 
+  function updatePlots(data) {
+    $('.js-plot').each(function () {
       const element = $(this).get(0)
+      const metricData = data.filter(obj => obj.metric === 'or' || obj.metric === 'rr')
 
-      return getData(query).then(function (data) {
-        data.forEach((obj) => {
-          element.setAttribute(`data-${obj.metric_category}`, obj.value)
-        })
-      })
+      function setPlotValue(element, data, category) {
+        const categoryData = metricData.filter(obj => obj.metric_category === category)
+        const categorySum = categoryData.reduce((acc, obj) => acc + obj.value, 0)
+        const categoryCount = categoryData.length
+        element.setAttribute(`data-${category}`, categorySum / categoryCount)
+      }
+
+      setPlotValue(element, metricData, 'or')
+      setPlotValue(element, metricData, 'or_ci_low')
+      setPlotValue(element, metricData, 'or_ci_upp')
+
+      setPlotValue(element, metricData, 'rr')
+      setPlotValue(element, metricData, 'rr_ci_low')
+      setPlotValue(element, metricData, 'rr_ci_upp')
     })
 
-    Promise.all(promises).then(refreshForestPlots)
+    refreshForestPlots()
   }
 
-  $('.dropdown-metric').on('change', updateData)
-  updateData()
-
-  $('.dropdown-metric').on('change', updatePlots)
-  updatePlots()
+  $('.dropdown-metric').on('change', fetchData)
+  fetchData()
 })
 
 } catch (error) {
