d2cdeb4c50ad98193e73539502330a112540242b
Graeme Porteous <graeme@rgbp.co.uk>
2023-06-13 06:56:29 +0100

    Move area data processing into area.js

--- static/js/area.js
+++ static/js/area.js
@@ -1,4 +1,6 @@
 import $ from '../vendor/jquery/js/jquery.esm.js'
+import { getArea, getData } from './data.js'
+import { updateAll as refreshForestPlots } from './forest-plots.js'
 
 $(function(){
   let dropdownMetric = $('.dropdown-metric')
@@ -51,3 +53,127 @@ function updateValues() {
 
   $('.metric-year').text($("#year option:selected").text())
 }
+
+const areaPage = function() {
+  $('.js-area-name').text(area.name)
+
+  $('[data-area-type]').each(function() {
+    const element = $(this).get(0)
+    if (element.getAttribute('data-area-type') === params.get('type')) {
+      element.removeAttribute('hidden')
+    } else {
+      element.setAttribute('hidden', 'hidden')
+    }
+  })
+
+  function fetchData() {
+    const conditions = { 'areas.id = ?': area.id }
+    const year = $("#year").val()
+    if (year != '') { conditions['date = ?'] = year }
+
+    // Fetch all data for an area for a given year
+    getData(conditions).then(function (data) {
+      updateData(data)
+      updatePlots(data)
+    })
+  }
+
+  function updateData(data) {
+    $('.js-data').each(function () {
+      console.debug('---------')
+
+      const metric = $(this).data('metric')
+      console.debug('metric', metric)
+
+      const metricCategory = $(this).data('metric-category') || metric
+      console.debug('metricCategory', metricCategory)
+
+      const metricData = data.filter(obj => obj.metric === metric && (obj.metric_category === 'NA' || obj.metric_category === metricCategory))
+      console.debug('metricData', metricData)
+
+      const valueTypes = [...new Set(metricData.map(obj => obj.value_type))]
+      const type = (valueTypes.length > 1) ? $(this).data('type') : valueTypes[0]
+
+      const metricTypeData = (valueTypes.length > 1) ?
+        metricData.filter(obj => obj.value_type === type) :
+        metricData
+      const metricTypeSum = metricTypeData.reduce((acc, obj) => acc + obj.value, 0)
+      const metricTypeCount = metricTypeData.length
+      console.debug('metricTypeData', type, metricTypeData, metricTypeSum, metricTypeCount)
+
+      const ethnicity = $(this).data('ethnicity')
+      const ethnicityData = metricTypeData.filter(obj => obj.ethnicity === ethnicity && typeof obj.value === 'number')
+      const ethnicitySum = ethnicityData.reduce((acc, obj) => acc + obj.value, 0)
+      const ethnicityCount = ethnicityData.length
+      console.debug('ethnicity', ethnicity, ethnicityData, ethnicitySum, ethnicityCount)
+
+      const ethnicityOtherData = metricTypeData.filter(obj => obj.ethnicity !== ethnicity && typeof obj.value === 'number')
+      const ethnicityOtherSum = ethnicityOtherData.reduce((acc, obj) => acc + obj.value, 0)
+      const ethnicityOtherCount = ethnicityOtherData.length
+      console.debug('ethnicityOther', `!${ethnicity}`, ethnicityOtherData, ethnicityOtherSum, ethnicityOtherCount)
+
+      let value
+      const format = $(this).data('format') || type
+      switch (format) {
+        case 'comparison-percentage':
+          value = (((ethnicitySum / ethnicityCount) / (metricTypeSum / metricTypeCount)) * 50).toFixed()
+          value = `${value}%`
+          break
+        case 'percentage':
+          value = (ethnicitySum / ethnicityCount).toFixed(2)
+          value = `${value}%`
+          break
+        case 'ratio-as-percentage':
+          value = Math.abs(((metricTypeSum / metricTypeCount) - 1) * 100)
+          value = `${value.toFixed()}%`
+          break
+        case 'ratio-as-more-or-less':
+          value = (((metricTypeSum / metricTypeCount) - 1) * 100)
+          if (value == 0) { value = 'as'}
+          else if (value < 0) { value = 'less' }
+          else { value = 'more' }
+          break
+        case 'ratio':
+          value = (metricTypeSum / metricTypeCount).toFixed(2)
+          break
+        case 'frequency':
+          value = ethnicitySum.toLocaleString()
+          break
+      }
+
+      console.debug('value', value)
+
+      const target = $(this).data('target')
+      if (target == 'height') { $(this).css('height', value) }
+      else if (target == 'width') { $(this).css('width', value) }
+      else { $(this).text(value) }
+    })
+  }
+
+  function updatePlots(data) {
+    $('.js-plot').each(function () {
+      const element = $(this).get(0)
+      const metricData = data.filter(obj => obj.metric === 'or' || obj.metric === 'rr')
+
+      function setPlotValue(element, data, category) {
+        const categoryData = metricData.filter(obj => obj.metric_category === category)
+        const categorySum = categoryData.reduce((acc, obj) => acc + obj.value, 0)
+        const categoryCount = categoryData.length
+        element.setAttribute(`data-${category}`, categorySum / categoryCount)
+      }
+
+      setPlotValue(element, metricData, 'rr')
+      setPlotValue(element, metricData, 'rr_ci_low')
+      setPlotValue(element, metricData, 'rr_ci_upp')
+    })
+
+    refreshForestPlots()
+  }
+
+  $('.dropdown-metric').on('change', fetchData)
+  fetchData()
+}
+
+const params = new URLSearchParams(document.location.search)
+const area = await getArea({ 'id = ?': params.get('id'), 'type = ?': params.get('type') })
+if (area) { $(areaPage) }
